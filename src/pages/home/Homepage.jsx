e0a8ad1bbdb14cbc0b7003cd620d0c5944eafb81
Thekey213 <azdilinga@gmail.com>
2024-03-16 16:36:30 +0200

    Chnage the Homepage and Sidebar code, The removed the sidebar toggle functionality. In the homepage modified the margins

--- src/pages/home/Homepage.jsx
+++ src/pages/home/Homepage.jsx
@@ -1,23 +1,20 @@
-// eslint-disable-next-line no-unused-vars
 import React from "react";
 import Sidebar from "../../components/Sidebar";
- import Header from "../../components/header";
- import Notification from "../../components/notifications";
+import Header from "../../components/header";
+import Notification from "../../components/notifications";
 
 const Homepage = () => {
-
-    return(
-       <div>
-           
-            <div><Sidebar/><Header/>
-                <div className="pl-2 pr-2 pt-4">
-                    <Notification/>
-                </div>
-                </div>
-       </div>
-     
-    )
-    
-}
+  return (
+    <div style={{ display: "flex" }}>
+      <Sidebar />
+      <div style={{ marginLeft: "210px", flexGrow: 1 }}>
+        <Header />
+        <div style={{ marginTop: "20px", marginLeft: "40px", flexGrow: 1 }}className="pl-2 pr-2 pt-4">
+          <Notification />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Homepage;
